366189bbb81eb3d8cefd6c0bef2237f510c0c911
GARUD6209 <harshvardhachandravanshi@gmail.com>
2024-09-07 17:53:03 +0530

    initial commit of chat app

---
+++ src/main/java/com/garud/chatapp/ChatappApplication.java
@@ -0,0 +1,14 @@
+package com.garud.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class ChatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappApplication.class, args);
+	}
+
+}
